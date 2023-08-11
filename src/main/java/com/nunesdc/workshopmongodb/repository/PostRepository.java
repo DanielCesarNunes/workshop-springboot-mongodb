51c8ce1c18d61ebac78b37cb8ec5a671a2a15b90
Daniel Cesar Nunes <nunesdc@hotmail.com>
2023-08-11 15:12:24 -0300

    Consulta simples com query methods

--- src/main/java/com/nunesdc/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/nunesdc/workshopmongodb/repository/PostRepository.java
@@ -1,12 +1,15 @@
 package com.nunesdc.workshopmongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.nunesdc.workshopmongodb.domain.Post;
-import com.nunesdc.workshopmongodb.domain.User;
+
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
